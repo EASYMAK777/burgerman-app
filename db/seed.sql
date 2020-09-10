1a347c21bfc82487c24404180be6351c73ea7dbf
EASYMAK777 <jplocallawncare@gmail.com>
2020-09-10 14:43:56 -0400

    created tables for schema, created values for seeds.sql

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Crabby Patti");
+INSERT INTO burgers (burger_name) VALUES ("Krusty Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Good Burger");
